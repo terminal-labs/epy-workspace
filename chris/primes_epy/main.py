37abccc479e6fa23415c57ff593e53a656dbfb8f
cleonard <ccl@chrisleonard.com>
2022-04-06 17:19:30 -0500

    Shell epython app with primes

---
+++ chris/primes_epy/main.py
@@ -0,0 +1,9 @@
+from primes import primes
+
+
+def do_primes(n=12):
+    return primes(n)
+
+
+if __name__ == "__main__":
+    print(do_primes())
---
+++ chris/primes_epy/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(
+    name='primes_epy',
+    version='0.1',
+    packages=['primes_epy'],
+)
