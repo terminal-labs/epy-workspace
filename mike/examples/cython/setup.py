6db0dc2402d4b73d8b7ad2240cca52bd88599e6d
Michael Verhulst <michael@terminallabs.com>
2022-04-06 11:12:39 -0500

    Update setup.py

--- mike/examples/cython/setup.py
+++ mike/examples/cython/setup.py
@@ -2,8 +2,8 @@ from setuptools import setup
 from Cython.Build import cythonize
 
 setup(
-    name='cypack',
+    name='cythonexample',
     version='0.1',
-    packages=['cypack'],
-    ext_modules=cythonize("cypack/fib.pyx"),
+    packages=['cythonexample'],
+    ext_modules=cythonize("cythonexample/fib.pyx"),
 )
