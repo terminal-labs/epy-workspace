8e7dc9eba6d129626fab1afeaa9a065fff38eb99
cleonard <ccl@chrisleonard.com>
2022-04-06 16:58:48 -0500

    Updated setup file

--- mike/examples/cython/setup.py
+++ mike/examples/cython/setup.py
@@ -1,9 +1,23 @@
-from setuptools import setup
-from Cython.Build import cythonize
+import os
+from setuptools import setup, Extension
 
-setup(
-    name='cythonexample',
-    version='0.1',
-    packages=['cythonexample'],
-    ext_modules=cythonize("cythonexample/fib.pyx"),
-)
+# Set local env var CYTHON = True if working with Cython backend
+# Assumes (for now) pyodide if not CYTHON
+CYTHON = bool(os.environ.get("CYTHON", False))
+
+if CYTHON:
+    from Cython.Build import cythonize  # noqa
+
+    setup(
+        name='cythonexample',
+        version='0.1',
+        packages=['cythonexample'],
+        ext_modules=cythonize("cythonexample/fib.pyx"),
+    )
+else:
+    setup(
+        name='cythonexample',
+        version='0.1',
+        packages=['cythonexample'],
+        ext_modules = [Extension(name = "cypack.fib", sources = ["cypack/fib.c"])]
+    )
