8bebfd5ea77b6a2e8cc36fffc2035d50c89b90b4
cleonard <ccl@chrisleonard.com>
2022-04-09 17:57:34 -0500

    Updated .epy and setup.py

--- mike/epy_demo/epy01/setup.py
+++ mike/epy_demo/epy01/setup.py
@@ -1,13 +1,33 @@
-from setuptools import setup
+import os
+from setuptools import setup, Extension
 
-setup(
-    name="epy01",
-    version='0.0.1',
-    packages=['epy01'],
-    install_requires=[
-    ],
-    entry_points='''
-        [console_scripts]
-        demo=epy01.app:main
-    ''',
-)
+CYTHON = os.environ.get("CYTHON", False)
+
+if CYTHON:
+    from Cython.Build import cythonize  # noqa
+    setup(
+        ext_modules=cythonize("epy01/functions.pyx"),
+        name="epy01",
+        version='0.0.1',
+        packages=['epy01'],
+        install_requires=[
+        ],
+        entry_points='''
+            [console_scripts]
+            demo=epy01.app:main
+        ''',
+    )
+else:
+
+    setup(
+        ext_modules=[Extension("epy01.functions", ["epy01/functions.c"])],
+        name="epy01",
+        version='0.0.1',
+        packages=['epy01'],
+        install_requires=[
+        ],
+        entry_points='''
+            [console_scripts]
+            demo=epy01.app:main
+        ''',
+    )
