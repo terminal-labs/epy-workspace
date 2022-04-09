fbface12ccab7c90b52d74558b7e9d108cf51166
Michael Verhulst <michael@terminallabs.com>
2022-04-09 13:26:35 -0500

    Update app.py

--- mike/vms/pyodide_env/superbuild/app.py
+++ mike/vms/pyodide_env/superbuild/app.py
@@ -5,15 +5,30 @@ import hashlib
 
 from cli_passthrough import cli_passthrough
 
-def cli():
-    os.chdir("/vagrant/cython")
-    print("in ", os.getcwd())
+def remove(path):
+    if os.path.exists(path):
+        if os.path.isfile(path):
+            os.remove(path)
+        if os.path.isdir(path):
+            shutil.rmtree(path)
+
+def cythonexample(packagename):
+    appdir = "/vagrant/cython"
+    appversion = "0.1"
+    src_tarball = appdir + '/dist/' + packagename + '-' + appversion + '.tar.gz'
+    whl = packagename + '-' + appversion + "-cp310-cp310-emscripten_wasm32.whl"
+
+    pyodidehome = "/home/vagrant/pyodide"
+    package = pyodidehome + "/packages/" + packagename
+    metadata = pyodidehome + "/packages/" + packagename + "/meta.yaml"
+
+    os.chdir(appdir)
     os.environ["CYTHON"] = "True"
     cli_passthrough("python setup.py build_ext --inplace")
     cli_passthrough("python setup.py sdist bdist_wheel")
     del os.environ["CYTHON"]
 
-    with open('/vagrant/cython/dist/cythonexample-0.1.tar.gz', 'rb') as f:
+    with open(src_tarball, 'rb') as f:
         sha256_hash = hashlib.sha256()
         for byte_block in iter(lambda: f.read(4096),b""):
             sha256_hash.update(byte_block)
@@ -21,33 +36,31 @@ def cli():
 
     d = {
         "package":
-            {"name": "cythonexample", "version": "0.1"},
+            {"name": packagename, "version": appversion},
         'source':
-            {"url": "file:///vagrant/cython/dist/cythonexample-0.1.tar.gz", "sha256": hashsum}
+            {"url": "file://" + src_tarball, "sha256": hashsum}
         }
 
-    if not os.path.exists("/home/vagrant/pyodide/packages/cythonexample"):
-        os.mkdir("/home/vagrant/pyodide/packages/cythonexample")
+    if not os.path.exists(package):
+        os.mkdir(package)
 
-    if os.path.exists("/home/vagrant/pyodide/packages/cythonexample/meta.yaml"):
-        os.remove("/home/vagrant/pyodide/packages/cythonexample/meta.yaml")
-    with open('/home/vagrant/pyodide/packages/cythonexample/meta.yaml', 'w') as outfile:
+    if os.path.exists(metadata):
+        os.remove(metadata)
+    with open(metadata, 'w') as outfile:
         yaml.dump(d, outfile, default_flow_style=False)
 
-    os.chdir("/home/vagrant/pyodide/packages/cythonexample")
-    print("in ", os.getcwd())
-    if os.path.exists("dist"):
-        shutil.rmtree('dist')
-    if os.path.exists("build"):
-        shutil.rmtree('build')
+    os.chdir(package)
+    remove("dist")
+    remove("build")
     os.environ["PYTHONPATH"] = "$PYTHONPATH:/home/vagrant/pyodide/pyodide-build/"
     cli_passthrough("python -m pyodide_build buildpkg meta.yaml")
 
-    os.chdir("/home/vagrant/pyodide")
-    print("in ", os.getcwd())
-    os.environ["PYODIDE_PACKAGES"] = "cythonexample"
+    os.chdir(pyodidehome)
+    os.environ["PYODIDE_PACKAGES"] = packagename
     cli_passthrough("make")
 
-    if os.path.exists("/home/vagrant/pyodide/build/cythonexample-0.1-cp310-cp310-emscripten_wasm32.whl"):
-        os.remove("/home/vagrant/pyodide/build/cythonexample-0.1-cp310-cp310-emscripten_wasm32.whl")
-    shutil.copy('packages/cythonexample/dist/cythonexample-0.1-cp310-cp310-emscripten_wasm32.whl', 'build/cythonexample-0.1-cp310-cp310-emscripten_wasm32.whl')
+    remove(pyodidehome + "/build/" + whl)
+    shutil.copy(pyodidehome + '/packages/' + packagename + '/dist/' + whl, pyodidehome + '/build/')
+
+def cli():
+    cythonexample('cythonexample')
