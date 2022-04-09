dc7ef2f43cdec09cebb81f9ac02bd3a8d84cd3a2
cleonard <ccl@chrisleonard.com>
2022-04-09 12:26:14 -0500

    Reorganizing epy example directories

---
+++ mike/epy_demo/epy00/epy00/app.py
@@ -0,0 +1,5 @@
+from epy00.functions import hello_world, fib
+
+def main():
+    hello_world()
+    fib(100)
--- mike/epy_demo/epy_00/demopack/app.py
+++
@@ -1,5 +0,0 @@
-from demopack.functions import hello_world, fib
-
-def main():
-    hello_world()
-    fib(100)
