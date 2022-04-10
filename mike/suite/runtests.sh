e96820a592b4bc9d5b1788916c9a16d36fba7264
Michael Verhulst <michael@terminallabs.com>
2022-04-10 15:17:40 -0500

    Update runtests.sh

--- mike/suite/runtests.sh
+++ mike/suite/runtests.sh
@@ -2,11 +2,11 @@ source ~/miniconda3/etc/profile.d/conda.sh
 #conda create -y --name suite python=3.10
 conda activate suite
 
-# cd workspace
-# git clone git@github.com:terminal-labs/epython.git
-# vagrant up
-# cd epython
-# pip install -e .
-# cd ..
+cd workspace
+git clone git@github.com:terminal-labs/epython.git
+vagrant up
+cd epython
+pip install -e .
+cd ..
 pip install -r requirements.txt
 python client.py
